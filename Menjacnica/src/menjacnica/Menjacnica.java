4c0ff2a5753d59467e72c503a52e916425009e2c
JovanaLazic <jovanalazic9696@gmail.com>
2017-03-30 13:12:06 +0200

    Potpuno implementirane metode za dodavanje, brisanje i pretrzivanje valuta.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,27 +1,53 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import valute.Valute;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	private LinkedList<Valute> valute=new LinkedList<Valute>();
 
 	@Override
 	public void dodajKurs(String naziv, String skraceno, GregorianCalendar datum, double prodajni, double kupovni,
 			double srednji) {
-		// TODO Auto-generated method stub
+		Valute v=new Valute();
+		v.setNazivValute(naziv);
+		v.setSkraceniNaziv(skraceno);
+		v.setDatum(datum);
+		v.setKupovniKurs(kupovni);
+		v.setProdajniKurs(prodajni);
+		v.setSrednjiKurs(srednji);
+		
+		if(!(valute.contains(v))){
+			valute.addLast(v);
+			}
 
 	}
 
 	@Override
 	public void obrisiKurs(String naziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		Valute v=new Valute();
+		v.setNazivValute(naziv);
+		v.setDatum(datum);
+		for(int i=0;i<valute.size();i++){
+			if(valute.get(i).equals(v))
+				valute.remove(valute.get(i));
+		}
+
 
 	}
 
 	@Override
 	public Valute pronadjiKurs(String naziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		Valute v=new Valute();
+		v.setNazivValute(naziv);
+		v.setDatum(datum);
+		for(int i=0;i<valute.size();i++){
+			if(valute.get(i).equals(v))
+				return valute.get(i);
+				
+		}
 		return null;
 	}
 
